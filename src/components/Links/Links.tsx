e75b04f5302b0131a719cd2fc8217ce62489d52c
Egor <egor.lapin.workspace@gmail.com>
2023-06-06 20:13:33 +0300

    правки стилей небольшие

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -4,42 +4,40 @@ import { vk, telegram, youtube } from '../../assets';
 const Links: React.FC = () => {
   return (
     <section className={styles.links_section}>
-      <div className={styles.container}>
-        <div className={styles.h2_container}>
-          <h3>Ссылки на наши официальные социальные сети, присоединяйтесь:</h3>
+      <div className={styles.h2_container}>
+        <h3>Ссылки на наши официальные социальные сети, присоединяйтесь:</h3>
+      </div>
+
+      <div className={styles.container_links}>
+        <div className={styles.container_link}>
+          <a href="https://vk.com/mangazeya_development" target="blank">
+            <img src={vk} alt="ВК" />
+            <p>ВК ГК Мангазея</p>
+          </a>
         </div>
 
-        <div className={styles.container_links}>
-          <div className={styles.container_link}>
-            <a href="https://vk.com/mangazeya_development" target="blank">
-              <img src={vk} alt="ВК" />
-              <p>ВК ГК Мангазея</p>
-            </a>
-          </div>
+        <div className={styles.container_link}>
+          <a href="https://t.me/gk_mangazeya" target="blank">
+            <img src={telegram} alt="Телеграм ГК Мангазея" />
+            <p>Телеграм канал ГК Мангазея</p>
+          </a>
+        </div>
 
-          <div className={styles.container_link}>
-            <a href="https://t.me/gk_mangazeya" target="blank">
-              <img src={telegram} alt="Телеграм ГК Мангазея" />
-              <p>Телеграм канал ГК Мангазея</p>
-            </a>
-          </div>
+        <div className={styles.container_link}>
+          <a href="https://t.me/+6PYvy0FJsuJjMDAy" target="blank">
+            <img src={telegram} alt="Телеграм MNGZ TEAM" />
+            <p>Телеграм канал MNGZ TEAM</p>
+          </a>
+        </div>
 
-          <div className={styles.container_link}>
-            <a href="https://t.me/+6PYvy0FJsuJjMDAy" target="blank">
-              <img src={telegram} alt="Телеграм MNGZ TEAM" />
-              <p>Телеграм канал MNGZ TEAM</p>
-            </a>
-          </div>
-
-          <div className={styles.container_link}>
-            <a
-              href="https://www.youtube.com/channel/UCeYGWggXwV70gwpTKhslJFQ/videos"
-              target="blank"
-            >
-              <img src={youtube} alt="YouTube" />
-              <p>Youtube канал Мангазея</p>
-            </a>
-          </div>
+        <div className={styles.container_link}>
+          <a
+            href="https://www.youtube.com/channel/UCeYGWggXwV70gwpTKhslJFQ/videos"
+            target="blank"
+          >
+            <img src={youtube} alt="YouTube" />
+            <p>Youtube канал Мангазея</p>
+          </a>
         </div>
       </div>
     </section>
