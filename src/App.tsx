cf245d7efa589ee268d34e4eaaa072ea5f7bcb69
Egor <egor.lapin.workspace@gmail.com>
2023-06-15 12:37:23 +0300

    Адаптивность первого блока под мобильные устройства

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import styles from './App.module.scss';
 import Welcome from './components/Welcome/Welcome';
 import AboutCompany from './components/AboutCompany/AboutCompany';
-import Security from './components/Security/Security';
-import WiFi from './components/WiFi/WiFi';
-import InternalPortal from './components/InternalPortal/InternalPortal';
-import RulesTerminal from './components/RulesTerminal/RulesTerminal';
-import BaseSystems from './components/BaseSystems/BaseSystems';
+// import Security from './components/Security/Security';
+// import WiFi from './components/WiFi/WiFi';
+// import InternalPortal from './components/InternalPortal/InternalPortal';
+// import RulesTerminal from './components/RulesTerminal/RulesTerminal';
+// import BaseSystems from './components/BaseSystems/BaseSystems';
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <Welcome />
       <AboutCompany />
-      <Security />
+      {/* <Security />
       <WiFi />
       <InternalPortal />
       <RulesTerminal />
-      <BaseSystems />
+      <BaseSystems /> */}
     </div>
   );
 }
